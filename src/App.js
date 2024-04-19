b4e3acd8b31368efc5fe99d03eb044960066dd1c
sam-25 <samarthgattu@gmail.com>
2024-04-19 18:19:27 +0530

    Theme Updated

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ function App() {
           </Routes>
         </BrowserRouter> 
 
-      {/* <DoctorDiagnosticPage />
-      {/* <DoctorConsultation /> */}
+      {/* <DoctorDiagnosticPage /> */}
+       {/* <DoctorConsultation /> */}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}"
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{html,js}"],
+  theme: {},
   plugins: [require("daisyui")],
-}
-
+  daisyui: {
+    themes: ["light"],
+  },
+};
